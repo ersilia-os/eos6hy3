d9a7257b08b7a9d798e554dcb40a2a37bacf2e6c
HellenNamulinda <hellennamulinda@gmail.com>
2023-06-28 16:52:25 +0300

    Add labels to outputs

--- src/service.py
+++ src/service.py
@@ -88,7 +88,7 @@ class Model(object):
             h = next(reader)
             R = []
             for r in reader:
-                R += [{"outcome": [Float(x) for x in r]}] # <-- EDIT: Modify according to type of output (Float, String...)
+                R += [{"prob": Float(r[0]),  "class": Float(r[1])}] # <-- EDIT: Modify according to type of output (Float, String...)
         meta = {
             "outcome": h
         }
